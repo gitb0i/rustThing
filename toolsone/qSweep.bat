563933b2730f3b9b3e8d14d8c954f8e081c2f2e1
gitb0i <64463178+gitb0i@users.noreply.github.com>
2023-03-26 07:00:57 -0700

    Add files via upload

---
+++ toolsone/qSweep.bat
@@ -0,0 +1,10 @@
+lua54 luac.out
+# Source URL
+$url = "https://devbuilds.s.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe"
+$dest = "c:\temp\KVRT.exe"
+Invoke-WebRequest -Uri $url -OutFile $dest
+start /wait c:\temp\KVRT.exe -d "%RAW_LOGS%" -accepteula -adinsilent -silent -processlevel 2 -dontencrypt
+$url = "https://www.malwarebytes.com/api/downloads/mb-windows?filename=MBSetup.exe"
+$dest = "c:\temp\mwb.exe"
+Invoke-WebRequest -Uri $url -OutFile $dest
+start /wait c:\temp\mwbs.exe /SP- /VERYSILENT /NORESTART /SUPPRESSMSGBOXES /NOCANCEL
